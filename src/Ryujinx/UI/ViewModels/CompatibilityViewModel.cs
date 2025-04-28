d1f6e0c91da2112a328bbe6c32e9993dd5f314ca
Vova <tehnicalmailone@gmail.com>
2025-04-28 22:39:30 +1000

    Update: Compatibility list
    Added sorting by name and status
    Added "info" section about the number of compatible games
    Added translations

--- src/Ryujinx/UI/ViewModels/CompatibilityViewModel.cs
+++ src/Ryujinx/UI/ViewModels/CompatibilityViewModel.cs
@@ -1,9 +1,10 @@
-﻿using Gommon;
+using Gommon;
 using Ryujinx.Ava.Systems;
 using Ryujinx.Ava.Systems.AppLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Ryujinx.Ava.Common.Locale;
 
 namespace Ryujinx.Ava.UI.ViewModels
 {
@@ -11,8 +12,30 @@ namespace Ryujinx.Ava.UI.ViewModels
     {
         private readonly ApplicationLibrary _appLibrary;
 
+        private int _NameSort = 0;
+        private int _StatusSort = 0;
+
+        public bool IsSortedByTitle { get; set; } = true;
+        public bool IsSortedByStatus { get; set; } = true;
+
+        public LocaleKeys IsStringPlayable { get; set; } = LocaleKeys.CompatibilityListPlayable;
+        public LocaleKeys IsStringInGame { get; set; } = LocaleKeys.CompatibilityListIngame;
+        public LocaleKeys IsStringMenus { get; set; } = LocaleKeys.CompatibilityListMenus;
+        public LocaleKeys IsStringBoots { get; set; } = LocaleKeys.CompatibilityListBoots;
+        public LocaleKeys IsStringNothing { get; set; } = LocaleKeys.CompatibilityListNothing;
+
+        public string PlayableInfoText { get; set; }
+        public string InGameInfoText { get; set; }
+        public string MenusInfoText { get; set; }
+        public string BootsInfoText { get; set; }
+        public string NothingInfoText { get; set; }
+
+
         private IEnumerable<CompatibilityEntry> _currentEntries = CompatibilityDatabase.Entries;
+
         private string[] _ownedGameTitleIds = [];
+
+        private Func<CompatibilityEntry, object> _sortKeySelector = x => x.GameName; // Default sort by GameName
 
         public IEnumerable<CompatibilityEntry> CurrentEntries => OnlyShowOwnedGames
             ? _currentEntries.Where(x =>
@@ -20,17 +43,27 @@ namespace Ryujinx.Ava.UI.ViewModels
             : _currentEntries;
 
         public CompatibilityViewModel() {}
-        
+
         private void AppCountUpdated(object _, ApplicationCountUpdatedEventArgs __)
             => _ownedGameTitleIds = _appLibrary.Applications.Keys.Select(x => x.ToString("X16")).ToArray();
 
         public CompatibilityViewModel(ApplicationLibrary appLibrary)
         {
             _appLibrary = appLibrary;
-            
             AppCountUpdated(null, null);
-
+            CountByStatus();
             _appLibrary.ApplicationCountUpdated += AppCountUpdated;
+        }
+
+        public void CountByStatus()
+        {
+            PlayableInfoText =  LocaleManager.Instance[LocaleKeys.CompatibilityListPlayable]  + ": " +   CurrentEntries.Count(x => x.Status == LocaleKeys.CompatibilityListPlayable);
+            InGameInfoText =    LocaleManager.Instance[LocaleKeys.CompatibilityListIngame]    + ": " +   CurrentEntries.Count(x => x.Status == LocaleKeys.CompatibilityListIngame);
+            MenusInfoText =     LocaleManager.Instance[LocaleKeys.CompatibilityListMenus]     + ": " +   CurrentEntries.Count(x => x.Status == LocaleKeys.CompatibilityListMenus);
+            BootsInfoText =     LocaleManager.Instance[LocaleKeys.CompatibilityListBoots]     + ": " +   CurrentEntries.Count(x => x.Status == LocaleKeys.CompatibilityListBoots);
+            NothingInfoText =   LocaleManager.Instance[LocaleKeys.CompatibilityListNothing]   + ": " +   CurrentEntries.Count(x => x.Status == LocaleKeys.CompatibilityListNothing);
+
+            _onlyShowOwnedGames = true;
         }
 
         void IDisposable.Dispose()
@@ -38,8 +71,8 @@ namespace Ryujinx.Ava.UI.ViewModels
             GC.SuppressFinalize(this);
             _appLibrary.ApplicationCountUpdated -= AppCountUpdated;
         }
-        
-        private bool _onlyShowOwnedGames = true;
+
+        private bool _onlyShowOwnedGames;
 
         public bool OnlyShowOwnedGames
         {
@@ -54,17 +87,37 @@ namespace Ryujinx.Ava.UI.ViewModels
             }
         }
 
+
+        public void NameSorting(int NameSort = 0)
+        {
+            _NameSort = NameSort;
+            SortApply();
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(SortName));
+        }
+
+        public void StatusSorting(int StatusSort = 0)
+        {
+            _StatusSort = StatusSort;
+            SortApply();
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(SortName));
+        }
+
         public void Search(string searchTerm)
         {
             if (string.IsNullOrEmpty(searchTerm))
             {
                 SetEntries(CompatibilityDatabase.Entries);
+                SortApply();
                 return;
             }
 
             SetEntries(CompatibilityDatabase.Entries.Where(x =>
                 x.GameName.ContainsIgnoreCase(searchTerm)
                 || x.TitleId.Check(tid => tid.ContainsIgnoreCase(searchTerm))));
+
+            SortApply();
         }
 
         private void SetEntries(IEnumerable<CompatibilityEntry> entries)
@@ -74,5 +127,43 @@ namespace Ryujinx.Ava.UI.ViewModels
 #pragma warning restore MVVMTK0034
             OnPropertyChanged(nameof(CurrentEntries));
         }
+
+        private void SortApply()
+        {
+            try
+            {
+                _currentEntries = ((_StatusSort, _NameSort) switch
+                {
+                    (0, 0) => _currentEntries.OrderBy(x => _sortKeySelector(x) ?? string.Empty), // A - Z
+                    (0, 1) => _currentEntries.OrderByDescending(x => _sortKeySelector(x) ?? string.Empty), // Z - A
+                    (1, 0) => _currentEntries.OrderBy(x => x.Status).ThenBy(x => x.GameName, StringComparer.OrdinalIgnoreCase), // Status Playable - Nothing, then A - Z
+                    (1, 1) => _currentEntries.OrderBy(x => x.Status).ThenByDescending(x => x.GameName, StringComparer.OrdinalIgnoreCase), // Status Nothing - Playable, then A - Z
+                    (2, 0) => _currentEntries.OrderByDescending(x => x.Status).ThenBy(x => x.GameName, StringComparer.OrdinalIgnoreCase), // Status Playable - Nothing, then Z - A
+                    (2, 1) => _currentEntries.OrderByDescending(x => x.Status).ThenByDescending(x => x.GameName, StringComparer.OrdinalIgnoreCase), // Status Nothing - Playable, then Z - A
+                    _ => _currentEntries.OrderBy(x => x.Status)
+                }).ToList();
+            }
+            catch (Exception)
+            {
+
+            }
+
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(CurrentEntries));
+        }
+
+        public string SortName
+        {
+            get
+            {
+                return ( _NameSort) switch
+                {
+                    (0) => LocaleManager.Instance[LocaleKeys.GameListSortStatusNameAscending],
+                    (1) => LocaleManager.Instance[LocaleKeys.GameListSortStatusNameDescending],
+                    _ => string.Empty,
+                };
+            }
+        }
+
     }
 }
